d86f19a0b9a782f995f4e9fc0f68a99455dbd63b
Wesley Teubl <59846142+SimplyWesley@users.noreply.github.com>
2023-10-17 16:13:35 +0200

    Update footer.php

--- require/footer.php
+++ require/footer.php
@@ -4,9 +4,7 @@
     <div class="footer">
         <p class="footer-text">
             <a href="https://www.wesleyteubl.com" target="_blank">Wesley Teubl</a> |
-            <a href="https://www.justinkroeze.com" target="_blank">Justin Kroeze</a> |
-            <a href="mailto:169063@student.horizoncollege.nl">Jennifer Bot</a> |
-            <a href="mailto:140649@student.horizoncollege.nl">Mitch Andria Valero</a>
+            <a href="https://www.justinkroeze.com" target="_blank">Justin Kroeze</a>
         </p>
     </div>
 </footer>
